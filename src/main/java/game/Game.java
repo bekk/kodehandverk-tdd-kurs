e8f89d0f89e698a611fb082ba7224f1b5535bf8d
Esben Aarseth <Esben.Aarseth@bekk.no>
2015-11-09 19:28:12 +0100

    * Opprettet instanser av interfacer i Game. * Test-skeleton for GameTest.

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,5 +1,13 @@
 package game;
 
+import display.Display;
+import lightning.Lightning;
+import payment.Payment;
+import storage.HighScoreRepository;
+
 public class Game {
-	
+	private Display display;
+    private Lightning lightning;
+    private Payment payment;
+    private HighScoreRepository highScoreRepository;
 }
---
+++ src/test/java/game/GameTest.java
@@ -0,0 +1,24 @@
+package game;
+
+import org.junit.Test;
+
+public class GameTest {
+
+    /*
+
+    Her vil vi teste at et spill ikke kan starte uten at spillere er "registrert" i Enterprise bowling systemet.
+
+    @Test(expected = Exception.class)
+    public void should_throw_exception_when_no_players_present_at_gamestart(){
+
+    }
+    */
+
+    /*
+    Her vil vi teste at et spill starter opp dersom en eller flere spillere har registrert seg.
+     */
+    @Test
+    public void should_start_game_when_one_or_more_players_present_at_gamestart(){
+
+    }
+}
