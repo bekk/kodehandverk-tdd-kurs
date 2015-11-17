ed4bd036736804b8b629ca1dc00962db3ed761b4
Torstein Nicolaysen <tnicolaysen@gmail.com>
2015-11-17 20:03:31 +0100

    Testene kjører nå på OSX også + refactor

--- src/main/java/storage/FileHighScoreRepositoryConnector.java
+++ src/main/java/storage/FileHighScoreRepositoryConnector.java
@@ -1,26 +1,21 @@
 package storage;
 
-import com.javafx.tools.doclets.internal.toolkit.util.DocFinder;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import player.Player;
 
-import javax.annotation.Resources;
 import java.io.*;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public final class FileHighScoreRepositoryConnector {
-    final String FILE_NAME = "HighScoreDatabase.json";
+    final String FILE_NAME = "/HighScoreDatabase.json";
 
     private final JSONArray getCurrentHighscores() throws IOException, ParseException {
-        File file = new File(getClass().getClassLoader().getResource(FILE_NAME).getFile());
+        File file = new File(getDatabaseFile());
         JSONObject data = (JSONObject) new JSONParser().parse(new FileReader(file));
         JSONArray scores = (JSONArray) data.get("scores");
         return scores;
@@ -28,11 +23,12 @@ public final class FileHighScoreRepositoryConnector {
 
     public Map<Player, Integer> readDatabase() throws IOException, ParseException {
         Map<Player, Integer> highScores = new HashMap<Player, Integer>();
+
         JSONArray scores = getCurrentHighscores();
-        for(int i = 0; i< scores.size(); i++) {
+        for (int i = 0; i < scores.size(); i++) {
             JSONObject highScore = (JSONObject) scores.get(i);
             String name = (String) highScore.get("name");
-            int score = ((Long)highScore.get("score")).intValue();
+            int score = ((Long) highScore.get("score")).intValue();
             highScores.put(new Player(name), score);
         }
         return highScores;
@@ -42,16 +38,20 @@ public final class FileHighScoreRepositoryConnector {
         JSONObject topLevelObject = new JSONObject();
         JSONArray scores = new JSONArray();
 
-        for(Player player : highscores.keySet()) {
+        for (Player player : highscores.keySet()) {
             JSONObject obj = new JSONObject();
             obj.put("name", player.getName());
             obj.put("score", highscores.get(player));
             scores.add(obj);
         }
         topLevelObject.put("scores", scores);
-        OutputStream outputStream = new FileOutputStream(getClass().getClassLoader().getResource(FILE_NAME).getFile());
+        OutputStream outputStream = new FileOutputStream(getDatabaseFile());
 
         outputStream.write(topLevelObject.toJSONString().getBytes());
         outputStream.close();
+    }
+
+    private String getDatabaseFile() {
+        return getClass().getResource(FILE_NAME).getFile();
     }
 }
